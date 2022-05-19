76274f434189972bc6904db97ad970eeb9caadc7
Jorge Reis <43137845+OhJorgeReis@users.noreply.github.com>
2022-05-19 22:53:42 +0200

    we see her!!

--- js/webapp.js
+++ js/webapp.js
@@ -45,25 +45,50 @@ cameraTrigger.onclick = function () {
 
 window.addEventListener("load", cameraStart, false);
 
-interact(".draggable").draggable({
-  inertia: true,
-  autoScroll: true,
+var angleScale = {
+  angle: 0,
+  scale: 1,
+};
+var gestureArea = document.getElementById("gesture-area");
+var scaleElement = document.getElementById("scale-element");
+var resetTimeout;
 
-  listeners: {
-    move: dragMoveListener,
-  },
-});
+interact(gestureArea)
+  .gesturable({
+    listeners: {
+      start(event) {
+        angleScale.angle -= event.angle;
 
-function dragMoveListener(event) {
-  var target = event.target;
+        clearTimeout(resetTimeout);
+        scaleElement.classList.remove("reset");
+      },
+      move(event) {
+        // document.body.appendChild(new Text(event.scale))
+        var currentAngle = event.angle + angleScale.angle;
+        var currentScale = event.scale * angleScale.scale;
 
-  var x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
-  var y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
+        scaleElement.style.transform =
+          "rotate(" + currentAngle + "deg)" + "scale(" + currentScale + ")";
 
-  target.style.transform = "translate(" + x + "px, " + y + "px)";
+        // uses the dragMoveListener from the draggable demo above
+        dragMoveListener(event);
+      },
+      end(event) {
+        angleScale.angle = angleScale.angle + event.angle;
+        angleScale.scale = angleScale.scale * event.scale;
 
-  target.setAttribute("data-x", x);
-  target.setAttribute("data-y", y);
+        resetTimeout = setTimeout(reset, 50000);
+        scaleElement.classList.add("reset");
+      },
+    },
+  })
+  .draggable({
+    listeners: { move: dragMoveListener },
+  });
+
+function reset() {
+  scaleElement.style.transform = "scale(1)";
+
+  angleScale.angle = 0;
+  angleScale.scale = 1;
 }
-
-window.dragMoveListener = dragMoveListener;
