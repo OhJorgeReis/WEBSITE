9f0e5f830d5a8fd618a6cbccdfd16bf6b8860e95
Jorge Reis <43137845+OhJorgeReis@users.noreply.github.com>
2022-05-20 02:03:51 +0200

    ok

--- js/webapp.js
+++ js/webapp.js
@@ -53,38 +53,37 @@ var gestureArea = document.getElementById("gesture-area");
 var scaleElement = document.getElementById("scale-element");
 var resetTimeout;
 
-interact(gestureArea)
-  .gesturable({
-    listeners: {
-      start(event) {
-        angleScale.angle -= event.angle;
+interact(gestureArea).gesturable({
+  listeners: {
+    start(event) {
+      angleScale.angle -= event.angle;
 
-        clearTimeout(resetTimeout);
-        scaleElement.classList.remove("reset");
-      },
-      move(event) {
-        // document.body.appendChild(new Text(event.scale))
-        var currentAngle = event.angle + angleScale.angle;
-        var currentScale = event.scale * angleScale.scale;
-
-        scaleElement.style.transform =
-          "rotate(" + currentAngle + "deg)" + "scale(" + currentScale + ")";
-
-        // uses the dragMoveListener from the draggable demo above
-        dragMoveListener(event);
-      },
-      end(event) {
-        angleScale.angle = angleScale.angle + event.angle;
-        angleScale.scale = angleScale.scale * event.scale;
-
-        resetTimeout = setTimeout(reset, 50000);
-        scaleElement.classList.add("reset");
-      },
+      clearTimeout(resetTimeout);
+      scaleElement.classList.remove("reset");
     },
-  })
-  .draggable({
-    listeners: { move: dragMoveListener },
-  });
+    move(event) {
+      // document.body.appendChild(new Text(event.scale))
+      var currentAngle = event.angle + angleScale.angle;
+      var currentScale = event.scale * angleScale.scale;
+
+      scaleElement.style.transform =
+        "rotate(" + currentAngle + "deg)" + "scale(" + currentScale + ")";
+
+      // uses the dragMoveListener from the draggable demo above
+      dragMoveListener(event);
+    },
+    end(event) {
+      angleScale.angle = angleScale.angle + event.angle;
+      angleScale.scale = angleScale.scale * event.scale;
+
+      resetTimeout = setTimeout(reset, 50000);
+      scaleElement.classList.add("reset");
+    },
+  },
+});
+// .draggable({
+//   listeners: { move: dragMoveListener },
+// });
 
 function reset() {
   scaleElement.style.transform = "scale(1)";
