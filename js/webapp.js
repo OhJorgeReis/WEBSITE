3d82650a62d9a9832fd5d67606893b22a1003bbe
Jorge Reis <43137845+OhJorgeReis@users.noreply.github.com>
2022-05-25 09:48:39 +0200

    update on moving sticker

--- js/webapp.js
+++ js/webapp.js
@@ -75,37 +75,52 @@ var gestureArea = document.getElementById("gesture-area");
 var scaleElement = document.getElementById("scale-element");
 var resetTimeout;
 
-interact(gestureArea).gesturable({
-  listeners: {
-    start(event) {
-      angleScale.angle -= event.angle;
+interact(gestureArea)
+  .gesturable({
+    listeners: {
+      start(event) {
+        angleScale.angle -= event.angle;
 
-      clearTimeout(resetTimeout);
-      scaleElement.classList.remove("reset");
+        clearTimeout(resetTimeout);
+        scaleElement.classList.remove("reset");
+      },
+      move(event) {
+        // document.body.appendChild(new Text(event.scale))
+        var currentAngle = event.angle + angleScale.angle;
+        var currentScale = event.scale * angleScale.scale;
+
+        scaleElement.style.transform =
+          "rotate(" + currentAngle + "deg)" + "scale(" + currentScale + ")";
+
+        function dragMoveListener(event) {
+          var target = event.target;
+          // keep the dragged position in the data-x/data-y attributes
+          var x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
+          var y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
+
+          // translate the element
+          target.style.transform = "translate(" + x + "px, " + y + "px)";
+
+          // update the posiion attributes
+          target.setAttribute("data-x", x);
+          target.setAttribute("data-y", y);
+        }
+
+        // uses the dragMoveListener from the draggable demo above
+        dragMoveListener(event);
+      },
+      end(event) {
+        angleScale.angle = angleScale.angle + event.angle;
+        angleScale.scale = angleScale.scale * event.scale;
+
+        resetTimeout = setTimeout(reset, 1000);
+        scaleElement.classList.add("reset");
+      },
     },
-    move(event) {
-      // document.body.appendChild(new Text(event.scale))
-      var currentAngle = event.angle + angleScale.angle;
-      var currentScale = event.scale * angleScale.scale;
-
-      scaleElement.style.transform =
-        "rotate(" + currentAngle + "deg)" + "scale(" + currentScale + ")";
-
-      // uses the dragMoveListener from the draggable demo above
-      dragMoveListener(event);
-    },
-    end(event) {
-      angleScale.angle = angleScale.angle + event.angle;
-      angleScale.scale = angleScale.scale * event.scale;
-
-      resetTimeout = setTimeout(reset, 50000);
-      scaleElement.classList.add("reset");
-    },
-  },
-});
-// .draggable({
-//   listeners: { move: dragMoveListener },
-// });
+  })
+  .draggable({
+    listeners: { move: dragMoveListener },
+  });
 
 function reset() {
   scaleElement.style.transform = "scale(1)";
